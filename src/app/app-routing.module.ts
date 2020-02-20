8d8cd5e94e0b1c9de0e02e42209468dbe47934f9
LionelBeato <lionel.beato@gmail.com>
2020-02-20 10:52:31 -0500

    refactors routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OtherComponent } from './other/other.component';
+import { NameComponent } from './name/name.component';
 
 
 const routes: Routes = [
-  {path: 'other', component: OtherComponent}
+  {path: 'other', component: OtherComponent},
+  {path: '', component: NameComponent}
 ];
 
 @NgModule({
